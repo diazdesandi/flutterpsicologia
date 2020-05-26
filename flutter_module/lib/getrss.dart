012db770b6db25ecff5e22ca653ab4f68c25df6a
Jonathan HI <60914099+Rexfor@users.noreply.github.com>
2020-05-25 21:22:33 -0700

    RSS
    a ver

---
+++ flutter_module/lib/getrss.dart
@@ -0,0 +1,65 @@
+import 'dart:async';
+import 'package:webfeed/webfeed.dart';
+import 'package:http/http.dart' as http;
+
+
+class getrss {
+  static Future<List<Noticias>> Getrss() async {
+    var data = await http.get(
+        "http://actualidad.rt.com/feeds/all.rss",
+        headers: {
+          "Content-Type": "application/json"
+        });
+    var rssFeed = new RssFeed.parse(data.body.toString());
+
+    List<Noticias> noticias = new List(); //listado de noticias que se devuelven a la interfaz
+
+    for (int i = 0; i < rssFeed.items.length; i++) {
+      print("Titulo: " + rssFeed.items[i].title);
+      print("Descripcion: " + rssFeed.items[i].description);
+      print("Fecha: " + rssFeed.items[i].pubDate);
+      print("imagenes Url: " + rssFeed.items[i].enclosure.url);
+      print("Enlace a noticia: "+ rssFeed.items[i].link);
+      print("\n");
+
+      noticias.add(new Noticias(rssFeed.items[i].title, rssFeed.items[i].description, rssFeed.items[i].pubDate, rssFeed.items[i].enclosure.url, rssFeed.items[i].link));
+     }
+    return noticias;
+  }
+}
+
+class Noticias{
+  String _titulo,_descripcion,_fecha,_urlImage,_urlNoticias;
+
+  Noticias(this._titulo, this._descripcion, this._fecha, this._urlImage, this._urlNoticias);
+
+  get urlNoticias => _urlNoticias;
+
+  set urlNoticias(value) {
+    _urlNoticias = value;
+  }
+
+  get urlImage => _urlImage;
+
+  set urlImage(value) {
+    _urlImage = value;
+  }
+
+  get fecha => _fecha;
+
+  set fecha(value) {
+    _fecha = value;
+  }
+
+  get descripcion => _descripcion;
+
+  set descripcion(value) {
+    _descripcion = value;
+  }
+
+  String get titulo => _titulo;
+
+  set titulo(String value) {
+    _titulo = value;
+  }
+}
